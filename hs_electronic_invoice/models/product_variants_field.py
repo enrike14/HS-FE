1406a2f86dc6548b64f555b5b68283362ea437d3
enrike14 <evelasquez1485@gmail.com>
2022-05-07 09:29:36 -0500

    add variants fields

--- hs_electronic_invoice/models/product_variants_field.py
+++ hs_electronic_invoice/models/product_variants_field.py
@@ -5,23 +5,36 @@ from datetime import datetime
 
 
 class product_field(models.Model):
-	_inherit = "product.product"
-	#_inherit = "product.template"
-	#asignar campos al modulo de product.product
-	categoryProduct = fields.Selection(
-	[('Sin Categoría', 'Sin Categoría'),
-	('Materia prima Farmacéutica', 'Materia prima Farmacéutica'),
-	('Medicina', 'Medicina'),
-	('Alimento', 'Alimento')],string = 'Categoría del Producto')
-	fechaFabricacion = fields.Date(string='Fecha de Fabricación')
-	fechaCaducidad = fields.Date(string='Fecha de Caducidad')
-	codigoCPBSAbrev = fields.Char(string="CodigoCPBSAbrev")
-	codigoCPBS = fields.Char(string="CodigoCPBS")
-	unidadMedidaCPBS = fields.Char(string="UnidadMedidaCPBS")
-	codigoGTIN = fields.Char(string="CodigoGTIN")
-	codigoGTINInv = fields.Char(string="CodigoGTINInv")
-	tasaISC = fields.Char(string="Tasa ISC")
-	valorISC = fields.Char(string="Valor ISC")
-	tasaOTI = fields.Char(string="Tasa OTI")
-	valorTasa = fields.Char(string="Valor Tasa")
-	
+    _inherit = "product.product"
+    #_inherit = "product.template"
+    mensaje_codigo = 'Codigos tipo:\nGTIN – 14 (14 caracteres)\nGTIN – 13 (13 caracteres)\nGTIN – 12 (12 caracteres)\nGTIN – 8 (8 caracteres)'
+    # asignar campos al modulo de product.product
+    categoryProduct = fields.Selection(
+        [('Materia prima Farmacéutica', 'Materia prima Farmacéutica'),
+         ('Medicina', 'Medicina'),
+            ('Alimento', 'Alimento')], string='Categoría del Producto')
+    fechaFabricacion = fields.Date(string='Fecha de Fabricación')
+    fechaCaducidad = fields.Date(string='Fecha de Caducidad')
+    codigoCPBSAbrev = fields.Char(string="Código CPBS Abrev")
+    codigoCPBS = fields.Char(string="Código CPBS")
+    unidadMedidaCPBS = fields.Char(string="Unidad de Medida CPBS")
+    codigoGTIN = fields.Char(string="Código GTIN",
+                             size=14, help=mensaje_codigo)
+    codigoGTINInv = fields.Char(
+        string="Código GTIN para la unidad de inventario", size=14, help=mensaje_codigo)
+    tasaISC = fields.Float(string="Tasa ISC")
+    valorISC = fields.Float(string="Valor ISC")
+    tasaOTI = fields.Selection(
+        [('01', 'SUME 911'),
+         ('02', 'Tasa Portabilidad Numérica'),
+            ('03', 'Impuesto sobre seguro')], string='Tasa OTI')
+    valorTasa = fields.Float(string="Valor Tasa")
+    cantGTINCom = fields.Float(
+        string="	Cantidad del producto o servicio en el Código GTIN del ítem de comercialización")
+    cantGTINComInv = fields.Float(
+        string="	Cantidad del producto o servicio en el Código GTIN del ítem de comercialización (Inventario)")
+    unidadMedida = fields.Char(string="Unidad Medida")
+    infoItem = fields.Char(
+        string="Información de interés del emisor con respeto a un ítem de la FE")
+    precioAcarreo = fields.Float(string="Precio Acarreo")
+    precioSeguro = fields.Float(string="Precio Seguro")
