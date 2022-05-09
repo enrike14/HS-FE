aa550bd4b83064b7097d9ba5df427b648e85cc8e
enrike14 <evelasquez1485@gmail.com>
2022-05-09 16:37:25 -0500

    add logging info

--- hs_electronic_invoice/models/electronic_invoice_fields.py
+++ hs_electronic_invoice/models/electronic_invoice_fields.py
@@ -855,7 +855,7 @@ class electronic_invoice_fields(models.Model):
         logging.info("Transactions Values HS HERMEC" + str(transaction_values))
         response = requests.request(
             "POST", url, headers=headers, data=transaction_values)
-        logging.info('Info AZURE TRANSACTION DATA: ' + str(response.text))
+        logging.info('Info AZURE ALL VALUE DATA: ' + str(response.text))
         return json.loads(response.text)
 
     def get_array_payment_info(self):
