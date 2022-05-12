b2dfae41d2470ada0ca912a6e191560671254763
enrike14 <evelasquez1485@gmail.com>
2022-05-12 13:52:12 -0500

    msg transactions

--- hs_electronic_invoice/models/electronic_invoice_fields.py
+++ hs_electronic_invoice/models/electronic_invoice_fields.py
@@ -967,10 +967,10 @@ class electronic_invoice_fields(models.Model):
             'Content-Type': 'application/json',
             'Authorization': '{"client": "dev", "code": "123456"}'
         }
-        #logging.info("Transactions Values HS HERMEC" + str(transaction_values))
+        logging.info("Transactions Values HS HERMEC" + str(transaction_values))
         response = requests.request(
             "POST", url, headers=headers, data=transaction_values)
-        #logging.info('Info AZURE TRANSACTION DATA: ' + str(response.text))
+        logging.info('Info AZURE TRANSACTION DATA: ' + str(response.text))
         return json.loads(response.text)
 
     def get_client_info(self):
