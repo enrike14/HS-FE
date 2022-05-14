25c1bb3042ceca4ceabe26c915233c7429efdafa
ERILEX <edelgado@hermecsolutions.com>
2022-05-14 11:18:49 -0500

    add medidas,sistemas y comentarios

--- hs_electronic_invoice/models/electronic_invoice_measures.py
+++ hs_electronic_invoice/models/electronic_invoice_measures.py
@@ -10,6 +10,9 @@ class electronic_invoice_measures(models.Model):
 	_name = "electronic.invoice.measures"
 	name= fields.Char(string="Nombre",size=20)
 	symbol= fields.Char(string="Simbolo")
+	system=fields.Char(string="Sistema")
+	measures=fields.Char(string="Medida")
+	comments=fields.Char(string="Comentario")
 	def name_get(self):
 		result = []
 		for record in self:
