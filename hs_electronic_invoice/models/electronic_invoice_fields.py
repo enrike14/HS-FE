217f73573a9641a08a97283168501a82b6cc6cb0
enrike14 <evelasquez1485@gmail.com>
2022-05-16 12:07:43 -0500

    add pdf Value

--- hs_electronic_invoice/models/electronic_invoice_fields.py
+++ hs_electronic_invoice/models/electronic_invoice_fields.py
@@ -616,7 +616,7 @@ class electronic_invoice_fields(models.Model):
 
         response = requests.request(
             "POST", url, headers=headers, data=payment_values)
-        #logging.info('Resultado PDF:: ' + str(response.text))
+        logging.info('Resultado PDF:: ' + str(response.text))
         # return json.loads(response.text)
         #respuesta = json.loads(response)
         #logging.info("PD 64" + str(response))
