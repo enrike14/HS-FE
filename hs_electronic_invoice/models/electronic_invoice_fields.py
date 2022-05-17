64c10cf5df642348bf327822b5165ae97f8e98f5
enrike14 <evelasquez1485@gmail.com>
2022-05-17 15:46:10 -0500

    add user and password

--- hs_electronic_invoice/models/electronic_invoice_fields.py
+++ hs_electronic_invoice/models/electronic_invoice_fields.py
@@ -234,9 +234,17 @@ class electronic_invoice_fields(models.Model):
     def get_connection(self):
         url = self.hsfeURLstr + "api/token"
         files = []
-        headers = {}
-        payload = {'username': 'Hermec',
-                   'password': '123465'}
+        user = ""
+        password = ""
+        # constultamos el objeto de nuestra configuración del servicio
+        config_document_obj = self.env["electronic.invoice"].search(
+            [('name', '=', 'ebi-pac')], limit=1)
+        if config_document_obj:
+            user = config_document_obj.hsUser
+            password = config_document_obj.hsPassword
+
+        payload = {'username': user,
+                   'password': password}
 
         response = requests.request(
             "POST", url, headers=headers, data=payload, files=files)
@@ -257,8 +265,17 @@ class electronic_invoice_fields(models.Model):
         url = self.hsfeURLstr + "api/token"
         files = []
         headers = {}
-        payload = {'username': 'Hermec',
-                   'password': '123465'}
+        user = ""
+        password = ""
+        # constultamos el objeto de nuestra configuración del servicio
+        config_document_obj = self.env["electronic.invoice"].search(
+            [('name', '=', 'ebi-pac')], limit=1)
+        if config_document_obj:
+            user = config_document_obj.hsUser
+            password = config_document_obj.hsPassword
+
+        payload = {'username': user,
+                   'password': password}
 
         response = requests.request(
             "POST", url, headers=headers, data=payload, files=files)
