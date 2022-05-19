9648b78521ccb4a7409ae09d0a0dd0cbfa4dee0b
ERILEX <edelgado@hermecsolutions.com>
2022-05-19 11:48:35 -0500

    fix tipo contribuyente

--- hs_electronic_invoice/models/customers_fields.py
+++ hs_electronic_invoice/models/customers_fields.py
@@ -59,8 +59,6 @@ class customers_fields(models.Model):
 			self.tipoContribuyente='2'
 		elif str(self.TipoClienteFE)=='02' or str(self.TipoClienteFE)=='04':
 			self.tipoContribuyente='1'
-		else:
-			self.tipoContribuyente=''
 	
 	def _get_country_id(self):
 		self._cr.execute("SELECT id FROM res_country WHERE code LIKE 'PA' LIMIT 1")
