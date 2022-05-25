21508c5a02bb30a3218f8ebe5a8dcd75ea77908d
ERILEX <edelgado@hermecsolutions.com>
2022-05-25 14:05:32 -0500

    remove char

--- hs_electronic_invoice/models/electronic_invoice_province.py
+++ hs_electronic_invoice/models/electronic_invoice_province.py
@@ -3,7 +3,7 @@ from odoo import models, fields, api
 
 class electronic_invoice_province(models.Model):
 	_name = 'electronic.invoice.province'
-	code = fields.Integer(string='Código', required=True, translate=True)
+	code = fields.Char(string='Código', size=3, required=True, translate=True)
 	name = fields.Char(string='Nombre', size=255, required=True, translate=True)
 	country_id = fields.Many2one('res.country', string='País', required=False, translate=True, compute='_get_country_id', store=True, ondelete='cascade')
 	district_ids = fields.One2many('electronic.invoice.district', 'province_id', string='Distritos')
